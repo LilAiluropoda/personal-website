ac3d5007bc2b2e7226d2e08f21114afbd154ea67
LilAiluropoda <bcheng8798@gmail.com>
2024-05-24 02:38:39 +0800

    feat: add background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	:global(html) {
+		background-color: #3c3836;
+		background-size: 2.5rem 2.5rem;
+		background-image: linear-gradient(to right, rgba(168, 153, 132, 0.3) 0.3px, transparent 0.3px),
+			linear-gradient(to bottom, rgba(168, 153, 132, 0.3) 0.3px, transparent 0.3px);
+		background-position: -1rem -1rem;
+	}
+</style>
