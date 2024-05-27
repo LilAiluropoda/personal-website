20df0f5cf60093194f88fb72755a9d33fad7c268
LilAiluropoda <bcheng8798@gmail.com>
2024-05-27 15:18:08 +0800

    feat: add signature, homepage text

---
+++ src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<!-- NavBar -->
+<div class="sticky top-0 flex justify-center items-center w-full p-[0.625rem]">
+	<div
+		class="flex flex-row justify-between items-center w-full px-[1.25rem] py-[0.625rem] rounded-[0.3125rem] bg-[#282828]"
+	>
+		<!-- UserName -->
+		<button>
+			<div class="flex flex-row">
+				<span class="text-[2.25rem] text-[#fe8019]"> Lil </span>
+				<span class="text-[2.25rem] text-[#ebdbb2]"> Ailuropoda </span>
+			</div>
+		</button>
+		<!-- BurgerMenu Button -->
+		<button>
+			<div class="flex justify-center items-center">
+				<svg
+					width="50"
+					height="50"
+					viewBox="0 0 50 50"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M10.4167 25H39.5833M10.4167 14.5833H39.5833M10.4167 35.4167H39.5833"
+						stroke="#A89984"
+						stroke-width="4.16667"
+						stroke-linecap="round"
+					/>
+				</svg>
+			</div>
+		</button>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import Navbar from '$lib/components/layout/Navbar.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 </script>
 
-<slot />
+<div class="flex flex-col justify-between w-full min-h-[100vh]">
+	<Navbar />
+	<slot />
+	<Footer />
+</div>
